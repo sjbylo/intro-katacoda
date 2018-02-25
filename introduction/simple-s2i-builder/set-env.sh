a22ccf217c826c4b4c5e95986e71845ba4bbd7e2
Stephen <stephenbylo@gmail.com>
2018-02-25 20:49:19 +0800

    mod

--- introduction/simple-s2i-builder/set-env.sh
+++ introduction/simple-s2i-builder/set-env.sh
@@ -2,20 +2,15 @@
 
 echo Adding scenario data...
 
-#exec > .setup.log 2>&1
+exec > .setup.log 2>&1
 
 for i in {1..20}; do oc policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done
+
+for i in {1..20}; do test -f assets.tar && break || sleep 1; done
+tar xf assets.tar && rm -f assets.tar
+
+docker pull docker.io/openshift/base-centos7
 
 #while ! test -f assets.tgz; do sleep 1; done; tar xzf assets.tgz
 #git clone https://github.com/sjbylo/intro-katacoda.git .tmp && cp -frp .tmp/introduction/simple-s2i-builder/assets/* . && rm -rf .tmp/
 
-for i in {1..20}; do test -f assets.tar && break || sleep 1; done
-tar xf assets.tar
-
-ls -la
-
-#docker pull centos/python-35-centos7:latest
-#docker pull openshiftroadshow/parksmap-katacoda:1.0.0
-#oc import-image openshift/base-centos7 --confirm -n openshift
-
-docker pull openshift/base-centos7
