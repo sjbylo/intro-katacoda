397f6ac0fe35b12cc2e55ea18a7db21c313ab306
Stephen <stephenbylo@gmail.com>
2018-02-25 18:45:20 +0800

    mod

--- introduction/simple-s2i-builder/set-env.sh
+++ introduction/simple-s2i-builder/set-env.sh
@@ -6,12 +6,12 @@ set -x
 
 #exec > .setup.log 2>&1
 
-for i in {1..50}; do oc policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done
+for i in {1..20}; do oc policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done
 
 #while ! test -f assets.tgz; do sleep 1; done; tar xzf assets.tgz
 #git clone https://github.com/sjbylo/intro-katacoda.git .tmp && cp -frp .tmp/introduction/simple-s2i-builder/assets/* . && rm -rf .tmp/
 
-for i in {1..50}; do test -f assets.tar && break || sleep 1; done
+for i in {1..20}; do test -f assets.tar && break || sleep 1; done
 tar xf assets.tar
 
 #docker pull centos/python-35-centos7:latest
