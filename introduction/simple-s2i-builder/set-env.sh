e4f18f973504f3398d118acca7ce94d0a10e5fbc
Stephen <stephenbylo@gmail.com>
2018-02-26 09:42:57 +0800

    mod

--- introduction/simple-s2i-builder/set-env.sh
+++ introduction/simple-s2i-builder/set-env.sh
@@ -1,8 +1,11 @@
+# These lines get piped and echoed into the actual console!
 ~/.launch.sh
 
-echo Adding scenario data...
+stty -echo
+export PS1=""
+#clear
 
-#exec > .setup.log 2>&1
+echo Adding scenario data...
 
 for i in {1..20}; do oc policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done
 
@@ -14,3 +17,7 @@ docker pull docker.io/openshift/base-centos7
 #while ! test -f assets.tgz; do sleep 1; done; tar xzf assets.tgz
 #git clone https://github.com/sjbylo/intro-katacoda.git .tmp && cp -frp .tmp/introduction/simple-s2i-builder/assets/* . && rm -rf .tmp/
 
+#clear
+export PS1="$ "
+echo "Ready"
+stty echo
