f22d15e73881a28fac59e5b2c7c9bea2c33df848
Stephen <stephenbylo@gmail.com>
2018-02-26 10:45:09 +0800

    mod

--- introduction/simple-s2i-builder/env-init.sh
+++ introduction/simple-s2i-builder/env-init.sh
@@ -1,43 +1,20 @@
-#!/bin/bash -x
-
 ssh root@host01 "touch here0"
+ssh root@host01 "touch /root/here1"
 
-LOG=/tmp/run.log
+pwd | ssh root@host01 "cat >> commands"
+ssh root@host01 "echo >> commands"
+id | ssh root@host01 "cat >> commands"
+ssh root@host01 "echo >> commands"
+ls -la | ssh root@host01 "cat >> commands"
+ssh root@host01 "echo >> commands"
 
-exec >$LOG 2>&1
 
-function finish {
-	scp $LOG root@host01:
-}
-trap finish EXIT
+#LOG=/tmp/run.log
 
-#----------------------------------
-cat > /tmp/setup.sh <<END
-#!/bin/bash -x
-#exec > .setup.log 2>&1
+#exec >$LOG 2>&1
 
-for i in {1..50}; do oc policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done
-
-#while ! test -f assets.tgz; do sleep 1; done; tar xzf assets.tgz
-#git clone https://github.com/sjbylo/intro-katacoda.git .tmp && cp -frp .tmp/introduction/simple-s2i-builder/assets/* . && rm -rf .tmp/
-
-for i in {1..50}; do test -f assets.tar && break || sleep 1; done
-tar xf assets.tar
-
-#docker pull centos/python-35-centos7:latest
-#docker pull openshiftroadshow/parksmap-katacoda:1.0.0
-#oc import-image openshift/base-centos7 --confirm -n openshift
-
-docker pull openshift/base-centos7
-END
-#----------------------------------
-
-ssh root@host01 "touch here1"
-
-scp /tmp/setup.sh root@host01:.setup.sh
-ssh root@host01 "touch here2"
-ssh root@host01 "bash -x .setup.sh > ~/.setup.log 2>&1"
-ssh root@host01 "touch here3"
-
-#wait
+#function finish {
+#	scp $LOG root@host01:
+#}
+#trap finish EXIT
 
