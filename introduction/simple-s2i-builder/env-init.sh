09ea4fc584867c580eecda1e4375259112491eba
Stephen <stephenbylo@gmail.com>
2018-02-26 16:33:27 +0800

    mod

--- introduction/simple-s2i-builder/env-init.sh
+++ introduction/simple-s2i-builder/env-init.sh
@@ -27,24 +27,24 @@ find . >> $LOG 2>&1
 scp $LOG root@host01:
 
 #----------------------------------
-cat > /tmp/setup.sh <<END
-#!/bin/bash -x
-#exec > .setup.log 2>&1
-
-for i in {1..50}; do oc policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done
-
-#while ! test -f assets.tgz; do sleep 1; done; tar xzf assets.tgz
-#git clone https://github.com/sjbylo/intro-katacoda.git .tmp && cp -frp .tmp/introduction/simple-s2i-builder/assets/* . && rm -rf .tmp/
-
-for i in {1..50}; do test -f assets.tar && break || sleep 1; done
-tar xf assets.tar
-
-#docker pull centos/python-35-centos7:latest
-#docker pull openshiftroadshow/parksmap-katacoda:1.0.0
-#oc import-image openshift/base-centos7 --confirm -n openshift
-
-docker pull openshift/base-centos7
-END
+# SJB cat > /tmp/setup.sh <<END
+# SJB #!/bin/bash -x
+# SJB #exec > .setup.log 2>&1
+# SJB 
+# SJB for i in {1..50}; do oc policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done
+# SJB 
+# SJB #while ! test -f assets.tgz; do sleep 1; done; tar xzf assets.tgz
+# SJB #git clone https://github.com/sjbylo/intro-katacoda.git .tmp && cp -frp .tmp/introduction/simple-s2i-builder/assets/* . && rm -rf .tmp/
+# SJB 
+# SJB for i in {1..50}; do test -f assets.tar && break || sleep 1; done
+# SJB tar xf assets.tar
+# SJB 
+# SJB #docker pull centos/python-35-centos7:latest
+# SJB #docker pull openshiftroadshow/parksmap-katacoda:1.0.0
+# SJB #oc import-image openshift/base-centos7 --confirm -n openshift
+# SJB 
+# SJB docker pull openshift/base-centos7
+# SJB END
 #----------------------------------
 
 scp /tmp/setup.sh root@host01:
