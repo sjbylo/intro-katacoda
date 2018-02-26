6e12c507b10e001ee5a5944518728c79c820b7eb
Stephen <stephenbylo@gmail.com>
2018-02-26 11:30:06 +0800

    mod

--- introduction/simple-s2i-builder/env-init.sh
+++ introduction/simple-s2i-builder/env-init.sh
@@ -8,13 +8,19 @@ ssh root@host01 "echo >> commands"
 ls -la | ssh root@host01 "cat >> commands"
 ssh root@host01 "echo >> commands"
 
+id > id
+scp id root@host01:
 
-#LOG=/tmp/run.log
+LOG=/tmp/run.log
 
-#exec >$LOG 2>&1
+ssh root@host01 "touch /root/here2"
+exec >$LOG 2>&1
+ssh root@host01 "touch /root/here3"
 
-#function finish {
-#	scp $LOG root@host01:
-#}
-#trap finish EXIT
+function finish {
+	scp $LOG root@host01:
+}
+trap finish EXIT
+
+find .
 
